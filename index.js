0bd24c2d5a7d6ef490ac4e38359506b5452298c5
choi-su-jung <tnwjd839200@gmail.com>
2022-01-12 00:55:58 +0900

    7강완료

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//서버에서 분석해서 가져올수있게함
+//application/x-www-form-urlencoded 타입으로 
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 
 const mongoose = require('mongoose')
@@ -12,6 +20,21 @@ mongoose.connect(
 app.get('/', (req, res) => {
   res.send('Hello World! 반가워요 헬로월드')
 })
+
+app.post('/register', (req, res) => {
+  
+  //회원가입할때 필요한 정보들 client에서 가져오면
+  //그것들을 데이터베이스에 넣는다.
+
+  const user = new User(req.body)
+  user.save((err, userInfo) =>{
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
